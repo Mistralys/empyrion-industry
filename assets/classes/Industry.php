02c14649479af0df64f44ce7b1cec0c91eb382d9
Mistralys <s.mordziol@gmail.com>
2020-06-27 10:16:09 +0200

    Added getPlaceables(); Added inline docs.

--- assets/classes/Industry.php
+++ assets/classes/Industry.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace EmpyrionIndustry;
 
 use EmpyrionIndustry\Materials\Materials;
+use EmpyrionIndustry\Placeables\Placeables;
 
 class Industry
 {
@@ -18,9 +19,27 @@ class Industry
     */
     private $storageFolder;
     
+   /**
+    * @var Placeables
+    */
+    private $placeables;
+    
     public function __construct(string $storageFolder)
     {
         $this->storageFolder = $storageFolder;
+        $this->placeables = new Placeables($this);
+    }
+    
+   /**
+    * Retrieves the placeables collection, which holds all
+    * available structures that some materials may be placed
+    * on (Base, Capital Vessel...).
+    * 
+    * @return Placeables
+    */
+    public function getPlaceables() : Placeables
+    {
+        return $this->placeables;
     }
     
     public function getStorageFolder() : string
@@ -28,6 +47,12 @@ class Industry
         return $this->storageFolder;
     }
     
+   /**
+    * Retrieves the materials collection that contains all
+    * materials that are available in the game.
+    * 
+    * @return Materials
+    */
     public function getMaterials() : Materials
     {
         if(!isset($this->materials))
